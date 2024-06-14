903d81f703f6aeeaeb070e8bed4a1b345784a678
louie <louie.lou.llaneta@gmail.com>
2024-06-14 17:35:58 +0800

    refactor(server): separated route handler for create album requests

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,7 +3,6 @@ import { Buffer } from 'node:buffer'
 import Fastify from 'fastify'
 import fs from 'node:fs'
 import fsPromise from 'node:fs/promises'
-import path from 'node:path'
 import { v4 as uuidv4 } from 'uuid'
 
 import DatabaseQueries from './database-queries'
@@ -11,9 +10,9 @@ import Globals from './globals'
 import HtmlTemplatePages from './html/template-pages'
 import JSONifyCookies from './utility/jsonify-cookies'
 import UtilsFile from './utility/file'
-import UtilsID from './utility/id'
 
 import AssetsRouteHandler from './route-handlers/assets'
+import CreateAlbumRequestHandler from './route-handlers/create-album'
 import FontsHandler from './route-handlers/fonts'
 import HomepageRouteHandler from './route-handlers/homepage'
 import LogInRequestHandler from './route-handlers/log-in'
@@ -74,50 +73,7 @@ SERVER.get("/assets/*", AssetsRouteHandler)
 SERVER.get("/fonts/*", FontsHandler)
 SERVER.post("/log-in", LogInRequestHandler)
 SERVER.post("/sign-up", SignUpRequestHandler)
-
-/**
- * The handler for clients creating a new album
- */
-SERVER.post("/new/album", async (request, reply) => {
-    let clientHasCookie = request.headers?.cookie ?? false
-    if (clientHasCookie) {
-        let cookies = JSONifyCookies(request.headers.cookie)
-        let withSessionId = cookies?.sessionid
-        if (withSessionId) {
-            if (await DatabaseQueries.IsSessionIdValid(cookies.sessionid)) {
-                let isWithContentType = typeof request.headers?.["content-type"] === 'string'
-                let isWithContentLength = request.headers?.["content-length"] !== undefined
-                if (isWithContentLength && isWithContentType) {
-                    let contentType = request.headers["content-type"].toLowerCase()
-                    if (contentType == "text/plain") {
-                        let album_name = (request.body as string)
-                        let username = await DatabaseQueries.GetUsernameBySessionID(cookies.sessionid)
-                        let albumid = UtilsID.GenerateAlbumId()
-                        await DatabaseQueries.RecordNewAlbum(username, albumid, album_name)
-                        reply.code(Globals.HttpStatusCode.Ok).send(albumid).type("text/plain")
-                        return
-                    }
-                    else /* if the content type is not text/plain */ {
-                        reply.code(Globals.HttpStatusCode.BadRequest)
-                        return
-                    }
-                }
-                else /* if the request did not give the content-length and content-type */ {
-                    reply.code(Globals.HttpStatusCode.BadRequest)
-                    return
-                }
-            }
-            else /* if the session id is not valid */ {
-                reply.code(Globals.HttpStatusCode.BadRequest)
-                return
-            }
-        }
-        else /* without session id */ {
-            reply.code(Globals.HttpStatusCode.Unauthorized)
-            return
-        }
-    }
-})
+SERVER.post("/new/album", CreateAlbumRequestHandler)
 
 function GeneratePhotoSessionToken(): string {
     return uuidv4()
