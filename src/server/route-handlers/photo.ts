89c132366781a56ae326f84f10bf93c37a806448
louie <louie.lou.llaneta@gmail.com>
2024-06-21 08:56:58 +0800

    server: refactor photo route handler

--- src/server/route-handlers/photo.ts
+++ src/server/route-handlers/photo.ts
@@ -4,7 +4,6 @@ import { FastifyReply } from 'fastify'
 import DatabaseQueries from '../database-queries'
 import { ExtendedFastifyRequest } from '../interfaces'
 import Globals from '../globals'
-import JSONifyCookies from '../utility/jsonify-cookies'
 import UtilsFile from '../utility/file'
 import fsPromise from 'fs/promises'
 
@@ -13,8 +12,7 @@ export default async function PhotoRouteHandler( request: ExtendedFastifyRequest
     if (await request.IsNotOnSession())
         return reply.code(Globals.HttpStatusCode.Unauthorized)
 
-    let cookies = JSONifyCookies(request.headers?.cookie)
-    let username = await DatabaseQueries.GetUsernameBySessionID(cookies.sessionid)
+    let username = await DatabaseQueries.GetUsernameBySessionID(request.cookies.sessionid)
     let photo_resource = ((request.params as any)?.photo_resource as string)
     let photoid = photo_resource.split('.')[0]
     let client_doesnt_own_photo_resource = !(await DatabaseQueries.CheckPhotoResourceOwnership(username, photoid))
@@ -23,16 +21,13 @@ export default async function PhotoRouteHandler( request: ExtendedFastifyRequest
         return reply.code(Globals.HttpStatusCode.Unauthorized)
 
     let true_path_to_photo_resource = `./built/${photo_resource}`
-    let photo_resource_exists = await UtilsFile.IsFileExisting(true_path_to_photo_resource)
+    let photo_resource_doesnt_exist = !(await UtilsFile.IsFileExisting(true_path_to_photo_resource))
     let photo_mime_type = UtilsFile.DeduceMimeTypeByFileExtension(true_path_to_photo_resource)
-    
-    if (photo_resource_exists) {
-        let photo = await fsPromise.readFile(true_path_to_photo_resource)
-        return reply.code(Globals.HttpStatusCode.Ok).type(photo_mime_type).send(photo)
-    }
 
-    else {
+    if (photo_resource_doesnt_exist)
         return reply.code(Globals.HttpStatusCode.NotFound)
-    }
+
+    let photo = await fsPromise.readFile(true_path_to_photo_resource)
+    return reply.code(Globals.HttpStatusCode.Ok).type(photo_mime_type).send(photo)
 
 }
