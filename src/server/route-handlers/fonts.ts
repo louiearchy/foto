251128b31de0f7731a8f082a3ede34075d4ed9da
louie <louie.lou.llaneta@gmail.com>
2024-06-21 08:53:57 +0800

    server: refactor font route handler

--- src/server/route-handlers/fonts.ts
+++ src/server/route-handlers/fonts.ts
@@ -9,16 +9,15 @@ import UtilsFile from '../utility/file'
 
 export default async function FontsRouteHandler( request: FastifyRequest, reply: FastifyReply ) {
     let true_path_to_font_resource_file = path.join("built/web/", request.url)
-    let font_resource_file_exists = await UtilsFile.IsFileExisting(true_path_to_font_resource_file)
-    if (font_resource_file_exists) {
-        let mime_type = UtilsFile.DeduceMimeTypeByFileExtension(true_path_to_font_resource_file)
-        if (mime_type) {
-            reply.type(mime_type)
-        }
-        let font_resource_file = await fsPromise.readFile(true_path_to_font_resource_file)
-        reply.send(font_resource_file)
+    let font_resource_file_doesnt_exist = !(await UtilsFile.IsFileExisting(true_path_to_font_resource_file))
+
+    if (font_resource_file_doesnt_exist)
+        return reply.code(Globals.HttpStatusCode.NotFound)
+
+    let mime_type = UtilsFile.DeduceMimeTypeByFileExtension(true_path_to_font_resource_file)
+    if (mime_type) {
+        reply.type(mime_type)
     }
-    else /* if the font resource does not exists */ {
-        reply.code(Globals.HttpStatusCode.NotFound)
-    }
+    let font_resource_file = await fsPromise.readFile(true_path_to_font_resource_file)
+    reply.send(font_resource_file)
 }
