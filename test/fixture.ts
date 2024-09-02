f0382a7eac47a85aa8cbe3168e05d7ef2d4a5478
louie <louie.lou.llaneta@gmail.com>
2024-09-02 18:12:51 +0800

    test: migrate clean db task and populate db with dummy data task
    We migrated these tasks from gulpjs to mocha global fixtures because we will not be using gulpjs anymore

---
+++ test/fixture.ts
@@ -0,0 +1,31 @@
+
+import childprocess from 'node:child_process'
+
+function RunSqlFile(path_to_sql_file): Promise<void> {
+    return new Promise((resolve, reject) => {
+        let sql_process = childprocess.spawn(`psql -d fotodb -f ${path_to_sql_file} --quiet`, { shell: true });
+        sql_process.on('exit', () => {
+            sql_process.removeAllListeners('error');
+            resolve();
+        });
+        sql_process.on('error', () => {
+            reject();
+        });
+    })
+}
+
+async function CleanDB() {
+    await RunSqlFile('src/reset-db.sql');
+}
+
+async function PopulateDbWithDummyData() {
+    await RunSqlFile('test/server/populate-with-data.sql');
+}
+
+export const mochaGlobalSetup = async function ()  {
+    console.log('cleaning test db');
+    await CleanDB()
+
+    console.log('populating db with dummy data...');
+    await PopulateDbWithDummyData()
+}
