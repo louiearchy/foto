7b9bf9c543281684f90e6a46c34c65ec9126b388
louie <louie.lou.llaneta@gmail.com>
2025-03-22 18:32:11 +0800

    server: give 404 Not Found for an invalid album id

--- src/server/route-handlers/album-name.ts
+++ src/server/route-handlers/album-name.ts
@@ -16,6 +16,11 @@ export default async function AlbumNameRouteHandler(request: ExtendedFastifyRequ
         return reply.code(Globals.HttpStatusCode.Unauthorized).send()
 
     let album_name = await DatabaseQueries.GetAlbumNameByItsAlbumID(albumid)
+    let album_id_is_invalid = !album_name
+
+    if (album_id_is_invalid)
+        return reply.code(Globals.HttpStatusCode.NotFound).send()
+
     return reply.code(Globals.HttpStatusCode.Ok).type("text/plain").send(album_name)
 
 }
